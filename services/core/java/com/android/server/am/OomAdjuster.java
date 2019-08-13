72e5377058558aab574c063071c564700964a476
Zhuo Fu <zfu@codeaurora.org>
2019-08-13 17:52:15 +0800

    cgroup follow for procs in the same cgroup.procs
    For Android processes, cgroup is managed by AMS, when AMS change cgroup
    of a task, AMS ignored processes in the same process group cgroup.procs
    if a foreground app call fork at JNI and then switch to background,
    children processes will keep foreground priority and out of control
    
    dex2oat is a real world example that foreground app call exec dex2oat to
    compile dexfile, when the parent process switch to background cgroup,
    dex2oat will not follow it, foreground cgroup allows background dex2oat
    hold huge of CPU resources, it may impact UX of foreground apps
    
    With this change, the cgroup of children processes will follow its
    parent if they are in the same cgroup.procs
    
    Properties to control:
    ro.vendor.qti.cgroup_follow.enable to enable/disable this feature
    ro.vendor.qti.cgroup_follow.dex2oat_only to use for all or dex2oat only
    
    Change-Id: Ibb7c4a451dfad6615a04d1f894ede22c7adb8ec9
    CRs-Fixed: 2139112

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -35,6 +35,7 @@ import static android.os.Process.THREAD_GROUP_DEFAULT;
 import static android.os.Process.THREAD_GROUP_RESTRICTED;
 import static android.os.Process.THREAD_GROUP_TOP_APP;
 import static android.os.Process.setProcessGroup;
+import static android.os.Process.setCgroupProcsProcessGroup;
 import static android.os.Process.setThreadPriority;
 import static android.os.Process.setThreadScheduler;
 
@@ -161,6 +162,10 @@ public final class OomAdjuster {
     private final ActivityManagerService mService;
     private final ProcessList mProcessList;
 
+    // Process in same process Group keep in same cgroup
+    boolean mEnableProcessGroupCgroupFollow = false;
+    boolean mProcessGroupCgroupFollowDex2oatOnly = false;
+
     OomAdjuster(ActivityManagerService service, ProcessList processList, ActiveUids activeUids) {
         mService = service;
         mProcessList = processList;
@@ -169,6 +174,9 @@ public final class OomAdjuster {
         mLocalPowerManager = LocalServices.getService(PowerManagerInternal.class);
         mConstants = mService.mConstants;
         mAppCompact = new AppCompactor(mService);
+
+            mEnableProcessGroupCgroupFollow = Boolean.parseBoolean(mPerf.perfGetProp("ro.vendor.qti.cgroup_follow.enable", "false"));
+            mProcessGroupCgroupFollowDex2oatOnly = Boolean.parseBoolean(mPerf.perfGetProp("ro.vendor.qti.cgroup_follow.dex2oat_only", "false"));
 
         // The process group is usually critical to the response time of foreground app, so the
         // setter should apply it as soon as possible.
@@ -181,7 +189,12 @@ public final class OomAdjuster {
             final int pid = msg.arg1;
             final int group = msg.arg2;
             try {
-                setProcessGroup(pid, group);
+                if (mEnableProcessGroupCgroupFollow) {
+                    final int uid = ((Integer)msg.obj).intValue();
+                    setCgroupProcsProcessGroup(uid, pid, group, mProcessGroupCgroupFollowDex2oatOnly);
+                } else {
+                    setProcessGroup(pid, group);
+                }
             } catch (Exception e) {
                 if (DEBUG_ALL) {
                     Slog.w(TAG, "Failed setting process group of " + pid + " to " + group, e);
@@ -1757,7 +1770,7 @@ public final class OomAdjuster {
                         break;
                 }
                 mProcessGroupHandler.sendMessage(mProcessGroupHandler.obtainMessage(
-                        0 /* unused */, app.pid, processGroup));
+                        0 /* unused */, app.pid, processGroup, Integer.valueOf(app.info.uid)));
                 try {
                     if (curSchedGroup == ProcessList.SCHED_GROUP_TOP_APP) {
                         // do nothing if we already switched to RT
